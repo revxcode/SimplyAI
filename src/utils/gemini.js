f81893b9eb0c265bd832b209f071a7bc97cb265c
Teguh Ersyarudin <150860402+revxcode@users.noreply.github.com>
2025-01-15 15:43:51 +0700

    Update gemini.js

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -14,7 +14,7 @@ export const useGeminiAI = () => {
 		})
 
 		const genAI = new GoogleGenerativeAI(API_KEY)
-		const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" })
+		const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-8b" })
 		const chat = model.startChat({
 			history: [
 				{
